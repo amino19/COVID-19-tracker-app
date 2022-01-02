f4517e79bb1554277e7c0eac70e86b998fdb6eb2
amino19 <anshumaankrprasad76@gmail.com>
2022-01-03 03:14:04 +0530

    ✨ Update pom.xml(covid-tracker)

--- src/main/java/io/javaminos/covidtracker/CovidTrackerApplication.java
+++ src/main/java/io/javaminos/covidtracker/CovidTrackerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class CovidTrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CovidTrackerApplication.class, args);
 	}
-
 }
